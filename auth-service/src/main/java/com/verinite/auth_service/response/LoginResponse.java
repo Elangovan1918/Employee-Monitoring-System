776eb6b4f10a8b4062a99400709b73e5b79c3fec
Elangovan Palaniappan <115521114+Elangovan1918@users.noreply.github.com>
2026-09-08 23:49:30 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/verinite/auth_service/response/LoginResponse.java
+++
@@ -1,7 +0,0 @@
-package com.verinite.auth_service.response;
-
-public record LoginResponse( String accessToken,
-                             String refreshToken,
-                             String tokenType,
-                             long expiresIn) {
-}
